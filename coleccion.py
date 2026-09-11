71411149b2b3e88a37fe49ecec7829786b897b7e
alejhop <a.rodriguez54@uniandes.edu.co>
2026-09-11 14:54:30 -0500

    Update coleccion.py

--- coleccion.py
+++ coleccion.py
@@ -1,5 +1,7 @@
 # backend/logica/coleccion.py
 
+#---------------------------------------------------------ELIMINAR
+
 def eliminarIngrediente(self, ingrediente_id):
     ingrediente = Ingrediente.query.get(ingrediente_id)
     if ingrediente is None:
@@ -12,3 +14,36 @@ def eliminarIngrediente(self, ingrediente_id):
     db.session.commit()
     
     return True
+
+#---------------------------------------------------------ELIMINAR
+
+
+#---------------------------------------------------------POPULARIDAD
+
+def generarReportePopularidad(self):
+
+    todos_los_ingredientes = Ingrediente.query.all()
+    
+    ingredientes_reporte = []
+    for ingrediente in todos_los_ingredientes:
+        total_recetas = Receta.query.filter_by(
+            ingrediente_id=ingrediente.id
+        ).count()
+        
+        ingredientes_reporte.append({
+            "id": ingrediente.id,
+            "nombre": ingrediente.nombre,
+            "tipo": ingrediente.tipo.name if ingrediente.tipo else None,
+            "unidad_medida": ingrediente.unidad_medida.name if ingrediente.unidad_medida else None,
+            "disponible": ingrediente.disponible,
+            "total_recetas": total_recetas
+        })
+    
+    total_recetas_sistema = Receta.query.count()
+    
+    return {
+        "total_ingredientes": len(todos_los_ingredientes),
+        "total_recetas_sistema": total_recetas_sistema,
+        "ingredientes": ingredientes_reporte
+    }
+#---------------------------------------------------------POPULARIDAD
